460fa33dac0bf0f5373d82d9354482ef15a7fcbf
FrontExtreme01 <frontend.extreme@gmail.com>
2024-10-30 11:58:43 -0500

    Fx: test rendimiento lcp

--- src/components/home/SliderHome.tsx
+++ src/components/home/SliderHome.tsx
@@ -7,19 +7,7 @@ import { Pagination, Autoplay } from 'swiper/modules';
 // Importar CSS Module
 import styles from '../../styles/slider.module.css';
 
-import img1 from '../../img/home/slider/amazonia-adventure-park-near-madrid-zipline.webp'
-import img2 from '../../img/home/slider/atletico-de-madrid-stadium-and-museum-tour.webp'
-import img3 from '../../img/home/slider/chambao-madrid-best-restaurant-to-eat-spanish-cuisine.webp'
-import img4 from '../../img/home/slider/iberian-ham-and-wine-tasting-in-madrid-tours-and-activities.webp'
-
-const slides = [
-    { id: 1, img: img1, title: "amazonia adventure" },
-    { id: 2, img: img2, title: "atletico madrid" },
-    { id: 3, img: img3, title: "chambao restaurant" },
-    { id: 3, img: img4, title: "iberian ham" }
-]
-
-export default function SliderHome() {
+export default function SliderHome({ slides }: any) {
     return (
         <div className="relative">
             <Swiper
@@ -39,9 +27,9 @@ export default function SliderHome() {
                 modules={[Pagination, Autoplay]}
                 className={`mySwiper ${styles.sliderHome}`}
             >
-                {slides.map((slide) => (
+                {slides.map((slide: any) => (
                     <SwiperSlide key={slide.id}>
-                        <img className={styles.imgSlider} src={slide.img.src} alt={slide.title} />
+                        <img className={styles.imgSlider} src={slide.img.src} alt={slide.title} loading={slide.carga} fetchPriority="high" />
                     </SwiperSlide>
                 ))}
             </Swiper>
