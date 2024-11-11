3ed075a271d0a97d35fc3f001cabe2a166c0bdd3
FrontExtreme01 <frontend.extreme@gmail.com>
2024-11-11 16:00:15 -0500

    Fx: test form in tsx

---
+++ src/components/tours/FormBokun.tsx
@@ -0,0 +1,19 @@
+import logo from '../../img/logos/favicon.webp';
+
+export default function FormBokun({ urlCalendar, price }: any) {
+
+    return (
+        <div className="w-full lg:w-4/12">
+            <div className="w-full p-10 shadow-md rounded-2xl">
+                <div className="flex justify-between flex-wrap space-y-5 md:space-y-0 mb-10">
+                    <div className="w-full md:w-8/12">
+                        <p className="text-slate-600 font-semibold text-md">Price from:</p>
+                        <p className="text-6xl font-bold text-sky-900">${price}<span className="text-xl font-thin text-slate-600">€ per person</span></p>
+                    </div>
+                    <img className="rounded-full h-20 w-20 object-cover" src={logo.src} alt="" />
+                </div>
+                <div className="bokunWidget" data-src={urlCalendar}></div>
+            </div>
+        </div>
+    );
+};
--- src/components/tours/Map.tsx
+++ src/components/tours/Map.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
-export default function Map({ url }: { url: string }) {
+export default function Map({ url, titleAct }: any) {
     const [showIframe, setShowIframe] = useState(false);
 
     return (
-        <div className="">
+        <div className="w-full h-96">
             {showIframe ? (
-                <iframe className="w-full h-96 rounded-2xl" src={url} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <iframe title={`ubicación de ${titleAct}`} className="w-full h-full rounded-2xl" src={url} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             ) : (
-                <button onClick={() => setShowIframe(true)} className="w-full h-96 rounded-2xl bg-gray-200 text-center py-2" style={{background: `linear-gradient(#00000044,#00000044), url('/preloaderMap.webp') center`, backgroundRepeat: 'no-repeat', backgroundSize:'cover'}}>
+                <button onClick={() => setShowIframe(true)} className="w-full h-full rounded-2xl bg-gray-200 text-center py-2" style={{background: `linear-gradient(#00000044,#00000044), url('/preloaderMap.webp') center`, backgroundRepeat: 'no-repeat', backgroundSize:'cover'}}>
                     <p className="font-bold text-white text-2xl">Ver ubicación</p>
                 </button>
             )}
