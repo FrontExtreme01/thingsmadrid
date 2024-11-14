dee634732564d19f2c89fd257e670cfd537b6ca8
FrontExtreme01 <frontend.extreme@gmail.com>
2024-11-14 08:38:40 -0500

    Fx: added config astro.config for site url SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thingsmadrid.netlify.app/', // Tu dominio público, e.g.: https://mi-sitio.dev/. Usado para generar sitemaps y URLs canónicas.
   integrations: [tailwind(), react(), mdx()]
 });
