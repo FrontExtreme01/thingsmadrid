a1826cf4155819c62436547b71a785b20a17ca52
FrontExtreme01 <frontend.extreme@gmail.com>
2024-11-14 16:00:04 -0500

    Fx: added library fancybox for gallerie detail tour/rest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://thingsmadrid.netlify.app/', // Tu dominio público, e.g.: https://mi-sitio.dev/. Usado para generar sitemaps y URLs canónicas.
+  site: 'https://thingsmadrid.netlify.app/',
   integrations: [tailwind(), react(), mdx()]
 });
--- src/components/tours/SliderGallery.tsx
+++ src/components/tours/SliderGallery.tsx
@@ -3,41 +3,53 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
 import { Navigation, Autoplay, EffectFade } from 'swiper/modules';
+import Fancybox from '../../utils/Fancybox';
 
 // Importar CSS Module
 import styles from '../../styles/gallery.module.css';
 
 export default function SliderGallery({ images, altTitle }: any) {
     return (
-        <div className="relative mb-10">
-            <div className={styles.nextGall}>
-                <i className="at-arrow-right"></i>
-            </div>
-            <div className={styles.prevGall}>
-                <i className="at-arrow-left"></i>
-            </div>
-            <Swiper
-                spaceBetween={5}
-                effect={'fade'}
-                navigation={{
-                    nextEl: `.${styles.nextGall}`,
-                    prevEl: `.${styles.prevGall}`,
-                    disabledClass: `${styles.disabled}`
-                }}
-                autoplay={{
-                    delay: 5000,
-                    disableOnInteraction: false,
-                }}
-                modules={[Navigation, Autoplay, EffectFade]}
-                className={`mySwiper ${styles.sliderGallery}`}
-            >
-                {images.map((image: { src: string }, index: number) => (
-                    <SwiperSlide key={index}>
-                        <img src={image.src} alt={altTitle} loading={index == 0 ? 'eager' : 'lazy'} fetchPriority={index == 0 ? 'high' : 'auto'} decoding="async" width={1920} height={1080} className={styles.imgGallery} />
-                    </SwiperSlide>
-                ))}
+        <Fancybox
+            options={{
+                Carousel: {
+                    infinite: false,
+                },
+            }}
+        >
+            <div className="relative mb-10">
+                <div className={styles.nextGall}>
+                    <i className="at-arrow-right"></i>
+                </div>
+                <div className={styles.prevGall}>
+                    <i className="at-arrow-left"></i>
+                </div>
+                <Swiper
+                    spaceBetween={5}
+                    effect={'fade'}
+                    navigation={{
+                        nextEl: `.${styles.nextGall}`,
+                        prevEl: `.${styles.prevGall}`,
+                        disabledClass: `${styles.disabled}`
+                    }}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    modules={[Navigation, Autoplay, EffectFade]}
+                    className={`mySwiper ${styles.sliderGallery}`}
+                >
+                    {images.map((image: { src: string }, index: number) => (
+                        <SwiperSlide key={index}>
+                            <img src={image.src} alt={altTitle} loading={index == 0 ? 'eager' : 'lazy'} decoding="async" width={1920} height={1080} className={styles.imgGallery} />
+                            <a data-fancybox="gallery" href={image.src}>
+                                <i className={`${styles.iconZoom} at-magnifying-glass-plus`}></i>
+                            </a>
+                        </SwiperSlide>
+                    ))}
 
-            </Swiper>
-        </div>
+                </Swiper>
+            </div>
+        </Fancybox>
     );
 }
