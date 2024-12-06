1a750044363adb129ac2671ddfeb6b198ae5663e
FrontExtreme01 <frontend.extreme@gmail.com>
2024-12-06 14:55:34 -0500

    Ft: added sitemap, robots files and meta property locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,21 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://thingsmadrid.netlify.app',
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(),
+  sitemap({
+    i18n: {
+      defaultLocale: 'es',
+      locales: {
+        en: 'en-US',
+        es: 'es-ES',
+      }
+    }
+  })
+  ],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
