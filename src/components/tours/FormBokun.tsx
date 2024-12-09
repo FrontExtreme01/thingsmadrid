9d212d2c602c4bdbb2d4dd9fb34f41c052fbc1a0
FrontExtreme01 <frontend.extreme@gmail.com>
2024-12-09 15:00:24 -0500

    Fx: test rendimiento

--- src/components/tours/FormBokun.tsx
+++ src/components/tours/FormBokun.tsx
@@ -1,27 +1,18 @@
-import React, { Suspense } from 'react';
-
 import { favicon } from "@/img/allImages";
 
-export default function FormBokun({ urlCalendar, price, i18n }: any) {
-    const LazyLoadBokunScript = React.lazy(() => import('@/utils/LoadBokunScript'));
+export default function FormBokun({ price, i18n }: any) {
 
     return (
-        <>
-            <Suspense fallback={<div>Loading...</div>}>
-                <LazyLoadBokunScript />
-            </Suspense>
-            <div className="w-full lg:w-4/12">
-                <div className="w-full p-10 shadow-md border border-slate-200 rounded-2xl">
-                    <div className="flex justify-between flex-wrap space-y-5 md:space-y-0 mb-10">
-                        <div className="w-full md:w-8/12">
-                            <p className="text-slate-600 font-semibold text-md">{i18n.TOUR_PAGE.DETAIL_TOUR.PRICE_FROM}:</p>
-                            <p className="text-6xl font-bold text-sky-900">${price}<span className="text-xl font-thin text-slate-600">€ /{i18n.TOUR_PAGE.DETAIL_TOUR.PER_PERSON}</span></p>
-                        </div>
-                        <img className="rounded-full h-20 w-20 object-cover" src={favicon.src} alt="" />
+        <div className="w-full lg:w-4/12">
+            <div className="w-full p-10 shadow-md border border-slate-200 rounded-2xl">
+                <div className="flex justify-between flex-wrap space-y-5 md:space-y-0 mb-10">
+                    <div className="w-full md:w-8/12">
+                        <p className="text-slate-600 font-semibold text-md">{i18n.TOUR_PAGE.DETAIL_TOUR.PRICE_FROM}:</p>
+                        <p className="text-6xl font-bold text-sky-900">${price}<span className="text-xl font-thin text-slate-600">€ /{i18n.TOUR_PAGE.DETAIL_TOUR.PER_PERSON}</span></p>
                     </div>
-                    <div className="bokunWidget" data-src={urlCalendar}></div>
+                    <img className="rounded-full h-20 w-20 object-cover" src={favicon.src} alt="" />
                 </div>
             </div>
-        </>
+        </div>
     );
 };
