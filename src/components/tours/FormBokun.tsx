f834fcfcb2ac6dd9a39df87b3811f9ff430ff2be
FrontExtreme01 <frontend.extreme@gmail.com>
2024-12-10 09:38:03 -0500

    Fx: Test rendimiento script bokun, accesibilidad a galeria modal

--- src/components/tours/FormBokun.tsx
+++ src/components/tours/FormBokun.tsx
@@ -1,18 +1,26 @@
 import { favicon } from "@/img/allImages";
+import { lazy, Suspense } from "react";
 
-export default function FormBokun({ price, i18n }: any) {
+export default function FormBokun({urlCalendar, price, i18n }: any) {
+    const LazyLoadBokunScript = lazy(() => import('@/utils/LoadBokunScript'));
 
     return (
-        <div className="w-full lg:w-4/12">
-            <div className="w-full p-10 shadow-md border border-slate-200 rounded-2xl">
-                <div className="flex justify-between flex-wrap space-y-5 md:space-y-0 mb-10">
-                    <div className="w-full md:w-8/12">
-                        <p className="text-slate-600 font-semibold text-md">{i18n.TOUR_PAGE.DETAIL_TOUR.PRICE_FROM}:</p>
-                        <p className="text-6xl font-bold text-sky-900">${price}<span className="text-xl font-thin text-slate-600">€ /{i18n.TOUR_PAGE.DETAIL_TOUR.PER_PERSON}</span></p>
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <LazyLoadBokunScript />
+            </Suspense>
+            <div className="hidden md:block w-full lg:w-4/12">
+                <div className="w-full p-10 shadow-md border border-slate-200 rounded-2xl">
+                    <div className="flex justify-between flex-wrap space-y-5 md:space-y-0 mb-10">
+                        <div className="w-full md:w-8/12">
+                            <p className="text-slate-600 font-semibold text-md">{i18n.TOUR_PAGE.DETAIL_TOUR.PRICE_FROM}:</p>
+                            <p className="text-6xl font-bold text-sky-900">${price}<span className="text-xl font-thin text-slate-600">€ /{i18n.TOUR_PAGE.DETAIL_TOUR.PER_PERSON}</span></p>
+                        </div>
+                        <img className="rounded-full h-20 w-20 object-cover" src={favicon.src} alt="" />
                     </div>
-                    <img className="rounded-full h-20 w-20 object-cover" src={favicon.src} alt="" />
+                    <div className="bokunWidget" data-src={urlCalendar}></div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
--- src/components/tours/SliderGallery.tsx
+++ src/components/tours/SliderGallery.tsx
@@ -2,13 +2,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
-import { Navigation, Autoplay, EffectFade } from 'swiper/modules';
+import { Navigation, Autoplay } from 'swiper/modules';
 import Fancybox from '@/utils/Fancybox';
 
 // Importar CSS Module
 import styles from '@/styles/gallery.module.css';
 
-export default function SliderGallery({ images, altTitle }: any) {
+export default function SliderGallery({ images, accessibility }: any) {
     return (
         <Fancybox
             options={{
@@ -36,13 +36,23 @@ export default function SliderGallery({ images, altTitle }: any) {
                         delay: 5000,
                         disableOnInteraction: false,
                     }}
-                    modules={[Navigation, Autoplay, EffectFade]}
+                    modules={[Navigation, Autoplay]}
                     className={`mySwiper ${styles.sliderGallery}`}
                 >
-                    {images.map((image: { src: string }, index: number) => (
+                    {images.map((image: any, index: number) => (
                         <SwiperSlide key={index}>
-                            <img src={image.src} alt={altTitle} loading={index == 0 ? 'eager' : 'lazy'} decoding="async" width={1920} height={1080} className={styles.imgGallery} />
-                            <a data-fancybox="gallery" href={image.src}>
+                            <img 
+                            className={styles.imgGallery} 
+                            src={image.src} 
+                            alt={accessibility.altImg} 
+                            loading={index == 0 ? 'eager' : 'lazy'} 
+                            decoding="async" 
+                            width="1920"
+                            height="1080"
+                            srcSet={`${image.src} 240w, ${image.src} 600w, ${image.src} 720w, ${image.src} 840w, ${image.src} 994w `}
+                            sizes={`(max-width: 360px) 240px, (max-width: 640px) 600px, (max-width: 768px) 720px, (max-width: 1024px) 840px,  ${image.width}px`}
+                            />
+                            <a data-fancybox="gallery" href={image.src} aria-label={accessibility.ariaLabelGallery}>
                                 <i className={`${styles.iconZoom} at-magnifying-glass-plus`}></i>
                             </a>
                         </SwiperSlide>
