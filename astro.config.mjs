2aaa49c9bb2b87ffbd11f4e63b968a6911f4bc8c
FrontExtreme01 <frontend.extreme@gmail.com>
2024-12-17 14:50:17 -0500

    Fx: test send mail con version netlify 5.5.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import partytown from '@astrojs/partytown';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://thingsmadrid.netlify.app',
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind(), react(), mdx(),
   partytown({
     config: {
---
+++ src/actions/index.ts
@@ -0,0 +1,27 @@
+import { ActionError, defineAction } from "astro:actions";
+
+import { Resend } from "resend";
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const server = {
+    send: defineAction({
+        accept: "form",
+        handler: async () => {
+            const { data, error } = await resend.emails.send({
+                from: "Madrid Contact <test@whattodoincancun.jhonnycanul.pro>",
+                to: ["frontend.extreme@gmail.com"],
+                subject: "Thanks for contacting us!",
+                html: "<strong>Si salio en produccion?</strong>",
+            });
+
+            if (error) {
+                throw new ActionError({
+                    code: "BAD_REQUEST",
+                    message: error.message,
+                });
+            }
+
+            return data;
+        },
+    }),
+};
